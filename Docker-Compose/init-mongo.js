7caa294fea994d54f9ff136fd35f2f9ab234a7d1
SoyTiyi <santiago.marmartinez@gmail.com>
2021-03-07 17:56:36 -0500

    fix docker compose and dockerfiles

---
+++ Docker-Compose/init-mongo.js
@@ -0,0 +1,12 @@
+db.createUser(
+    {
+        user: "soytiyi",
+        pwd: "Pascual2017",
+        roles: [
+            {
+                role: "readWrite",
+                db: "labcinco"
+            }
+        ]
+    }
+)
